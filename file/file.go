317d29634b26bd8dc59c9c0a75c96d480dc73276
Deep Majumder <deep.majumder2019@gmail.com>
2021-09-14 19:18:37 +0530

    Files can commit themselves Added check for DotFile path to be absolute

--- file/file.go
+++ file/file.go
@@ -2,8 +2,10 @@ package file
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 )
@@ -16,14 +18,17 @@ type DotFile struct {
 }
 
 func NewDotFile(path, mnemonic string) (*DotFile, error) {
+	if !filepath.IsAbs(path) {
+		return nil, fmt.Errorf("failed to create dot file: %s is not absolute path", path)
+	}
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to dot file")
+		return nil, errors.Wrap(err, "failed to create dot file")
 	}
 	var buf bytes.Buffer
 	_, err = io.Copy(&buf, file)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to dot file")
+		return nil, errors.Wrap(err, "failed to create dot file")
 	}
 	history := NewHistory(buf.String())
 	dotFile := &DotFile{
@@ -33,4 +38,23 @@ func NewDotFile(path, mnemonic string) (*DotFile, error) {
 		currentHistory: history,
 	}
 	return dotFile, nil
+}
+
+func (file *DotFile) AddCommit() (bool, error) {
+	osFile, err := os.Open(file.path)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to dot file")
+	}
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, osFile)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to dot file")
+	}
+	node := file.currentHistory.AddCommit(buf.String())
+	if node != nil {
+		return false, nil
+	} else {
+		file.currentHistory = node
+		return true, nil
+	}
 }
